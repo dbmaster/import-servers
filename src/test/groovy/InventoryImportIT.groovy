af762bd3532c7de22c9a3a77e4051ee6e1ed9a48
Slava Christin <slava@branegy.com>
2015-12-03 21:32:15 -0500

    Add test to import applications

--- src/test/groovy/InventoryImportIT.groovy
+++ src/test/groovy/InventoryImportIT.groovy
@@ -1,8 +1,12 @@
-import io.dbmaster.testng.BaseToolTestNGCase;
-import static org.testng.Assert.assertTrue;
+import io.dbmaster.testng.BaseToolTestNGCase
+import static org.testng.Assert.assertTrue
 
-import org.testng.annotations.Parameters;
+import org.testng.annotations.Parameters
 import org.testng.annotations.Test
+
+import com.branegy.files.FileService
+import com.branegy.files.FileReference
+import org.apache.commons.io.IOUtils
 
 public class InventoryImportIT extends BaseToolTestNGCase {
 
@@ -13,8 +17,9 @@ public class InventoryImportIT extends BaseToolTestNGCase {
                            "p_action"      : "Preview" ]
         tools.toolExecutor("inventory-import", parameters).execute()
     }
+/*    
     
-    
+    // https://github.com/dbmaster/inventory-import/issues/9
     @Test
     public void testIgnoreImportColumns() {
         def parameters = [ "p_excel_file"    : "import-connections.xslx",
@@ -23,5 +28,34 @@ public class InventoryImportIT extends BaseToolTestNGCase {
                            "p_action"        : "Preview" ]
         tools.toolExecutor("inventory-import", parameters).execute()
     }
-    
+*/    
+
+    @Test
+    public void testImportApplications() {
+        def fileService = dbm.getService(FileService.class)
+        FileReference file = null
+        def filename = "applications.xlsx"
+        try {
+            file = fileService.getFile(filename)
+            println "File ${p_filename} already exists. Replacing content"
+        } catch (EntityNotFoundApiException e) {
+            // this means file does not exists
+            file = fileService.createFile(filename, "inventory-import-test")
+        }
+        def outputStream = file.getOutputStream()
+        def in = new FileInputStream( new java.io.File(getTestResourcesDir(), filename) )
+        IOUtils.copy(in, outputStream)
+        in.close()
+        outputStream.close()
+
+        def parameters = [ "p_excel_file"    : filename,
+                           "p_object_type"   : "Application",
+                           // "p_field_mapping" : "InputColumn=",
+                           "p_action"        : "Preview" ]
+        tools.toolExecutor("inventory-import", parameters).execute()
+        
+        fileService.deleteFile(file)
+    }
+
+
 }
