e157294984a09ea0710d08fe62b77dd2f276c3d6
Slava Christin <slava@branegy.com>
2016-01-05 22:38:23 -0500

    Change location of description

--- src/main/resources/META-INF/convert.bat
+++ src/main/resources/META-INF/convert.bat
@@ -1,1 +1,3 @@
+rem see https://developer.github.com/v3/markdown/
+
 wget --header="Content-Type: text/plain" --debug --verbose --no-check-certificate --output-document=description.html  --post-file description.md https://api.github.com/markdown/raw 
